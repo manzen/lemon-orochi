ad2b73babb4162642a932dc7ea324fb8e5c6d567
manzen <ririn.eva0129@gmail.com>
2019-10-27 12:25:12 +0900

    ちょっと制御加える

--- app.js
+++ app.js
@@ -2,42 +2,36 @@ const express = require('express');
 const app = express();
 const SerialPort = require("serialport");
 const port = 3000;
-const arduinoCOMPort = "COM3";
 
 const player = require('play-sound')(opts = {})
 
-const audio = player.play('sample.mp3', function(err){
-    if (err && !err.killed) throw err
-});
-// audio.kill();
+// Download the helper library from https://jp.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+require('dotenv').config();
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const querystring = require('querystring');
 
-const arduinoSerialPort = new SerialPort(arduinoCOMPort, {
-    baudRate: 9600
-});
+// const word = "アイルビーバック";
+// const twiml = '<Response><Say voice="woman" language="ja-jp">' + word + '</Say></Response>';
+const url = "https://remon-orochi.s3-ap-northeast-1.amazonaws.com/illbeback.mp3";
+const twiml = '<Response><Play loop="1">' + url + '</Play></Response>';
 
-arduinoSerialPort.on('open',function() {
-    console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
+player.play('sample.mp3', { afplay: ['-v', 1 ] }, function(err){
+    if (err) throw err
+    client.calls
+        .create({
+            url: 'http://twimlets.com/echo?Twiml=' + querystring.escape(twiml),
+            to: process.env.TO,
+            from: process.env.FROM
+        })
+        .then(call => console.log(call.sid));
 });
 
 app.get('/', function (req, res) {
     return res.send('Working');
-});
-
-app.get('/:action', function (req, res) {
-
-    var action = req.params.action || req.param('action');
-
-    if(action == 'led'){
-        arduinoSerialPort.write("w");
-        return res.send('Led light is on!');
-    }
-    if(action == 'off') {
-        arduinoSerialPort.write("t");
-        return res.send("Led light is off!");
-    }
-
-    return res.send('Action: ' + action);
-
 });
 
 app.listen(port, function () {
--- serial.js
+++ serial.js
@@ -1,17 +1,13 @@
 require('dotenv').config();
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
+const player = require('play-sound')(opts = {})
 const client = require('twilio')(accountSid, authToken);
 const querystring = require('querystring');
-const player = require('play-sound')(opts = {})
-
-// audio.kill();
-
-const word = "アイルビーバック";
-const twiml = '<Response><Say voice="woman" language="ja-jp">' + word + '</Say></Response>';
+const url = "https://remon-orochi.s3-ap-northeast-1.amazonaws.com/illbeback.mp3";
+const twiml = '<Response><Play loop="1">' + url + '</Play></Response>';
 
 const PATH = '/dev/cu.usbmodem14401'
-const BOARD_RATE = 9600
 
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
@@ -19,27 +15,47 @@ const Readline = require('@serialport/parser-readline')
 const port = new SerialPort(PATH, { baudRate: 9600 })
 const parser = port.pipe(new Readline({ delimiter: '\n' }))
 
-let isFlag = false
-
 port.on('open', () => {
     console.log('serial port open')
 })
 
+let play_count = 0
+let audio = null
+
 parser.on('data', data => {
     console.log('data: ', data)
-    if(!isFlag) {
-        const audio = player.play('sample.mp3', function(err){
-            if (err && !err.killed) throw err
-        });
-        isFlag = true
+
+    let is_operation = true
+    let is_play = true
+    let pressure = 0
+    let volume = null
+
+    // 音量設定
+    if (!isNaN(pressure) && pressure > 0 && pressure < 500) { // 感圧センサーから強い圧力を感じたら、音量を大きく
+        volume = 10
+    } else { // 感圧センサーから弱い圧力を感じたら、音量を小さく
+        volume = 1
     }
 
-    client.calls
-        .create({
-            url: 'http://twimlets.com/echo?Twiml=' + querystring.escape(twiml),
-            to: process.env.TO,
-            from: process.env.FROM
-        })
-        .then(call => console.log(call.sid));
+    if (is_operation) {
+        play_count += 1
+        // 音声再生
+        if (is_play) {
+            // TODO　音声ファイルを出し分ける
+            audio = player.play('sample.mp3', { afplay: ['-v', volume ] }, function(err){
+                if (err) throw err
+                // 音声が停止したらTwilioAPIを呼び出して電話をかける
+                client.calls
+                    .create({
+                        url: 'http://twimlets.com/echo?Twiml=' + querystring.escape(twiml),
+                        to: process.env.TO,
+                        from: process.env.FROM
+                    })
+                    .then(call => console.log(call.sid));
+            });
+        } else if (!is_play && !audio) {
+            audio.kill()
+        }
+    }
 })
 
